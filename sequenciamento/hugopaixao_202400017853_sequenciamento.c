4f8b810c1e04908be180f095b49931533e4fa394
Hugo Paixao <hs8622436@gmail.com>
2025-12-15 22:37:34 -0300

    bora ver

--- sequenciamento/hugopaixao_202400017853_sequenciamento.c
+++ sequenciamento/hugopaixao_202400017853_sequenciamento.c
@@ -7,8 +7,8 @@ typedef struct {
     char cod[10];
     char** genes;
     int qtdGenes;
-    int percent;
-    int idx; // índice original para estabilidade
+    int porcentagem;
+    int idx;
 } Doenca;
 
 void calcular_tabela(int* k, char* P, int m) {
@@ -38,114 +38,87 @@ void KMP(int* k, int* R, char* T, char* P) {
     }
 }
 
-void merge(Doenca* arr, int l, int m, int r) {
-    int n1 = m - l + 1, n2 = r - m;
-    Doenca* L = (Doenca*)malloc(n1 * sizeof(Doenca));
-    Doenca* R = (Doenca*)malloc(n2 * sizeof(Doenca));
-    for (int i = 0; i < n1; i++) L[i] = arr[l + i];
-    for (int i = 0; i < n2; i++) R[i] = arr[m + 1 + i];
+void counting_sort(Doenca* A, Doenca* B , int n) {
+    int k = 101; // porcentagem maxima +1
+    int* C = (int*) caloc(k, sizeof(int));
 
-    int i = 0, j = 0, k = l;
-    while (i < n1 && j < n2) {
-        if (L[i].percent > R[j].percent) {
-            arr[k++] = L[i++];
-        } else if (L[i].percent < R[j].percent) {
-            arr[k++] = R[j++];
-        } else {
-            arr[k++] = (L[i].idx <= R[j].idx) ? L[i++] : R[j++];
-        }
+    for(int i = 0; i < n; i++)
+        C[A[i].porcentagem]++;
+
+    for(int i = 1; i < k; i++)
+        C[i] = C[i]+C[i-1];
+
+    for(int i = n-1; i >= 0; i--) {
+        B[C[A[i].porcentagem] -1] = A[i];
+        C[A[i].porcentagem]--;
     }
-    while (i < n1) arr[k++] = L[i++];
-    while (j < n2) arr[k++] = R[j++];
-    free(L);
-    free(R);
+    free(C);
+
+    for(int i = 0; i < n/2; i++) {
+        Doenca temp = B[i];
+        B[i] = B[n-1-i];
+        B[n-1-i] = temp;
+    }
+
 }
+Doenca* processarEntrada(FILE* input, int* K, char* Dna, int* M) {
+    fscanf(input, "%d", K);
+    fscanf(input, "%10004s", Dna);
+    fscanf(input, "%d", M);
 
-void mergesort(Doenca* arr, int l, int r) {
-    if (l < r) {
-        int m = l + (r - l) / 2;
-        mergesort(arr, l, m);
-        mergesort(arr, m + 1, r);
-        merge(arr, l, m, r);
-    }
-}
+    Doenca* d = (Doenca*)malloc((*M) * sizeof(Doenca));
 
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Uso: %s <entrada> <saida>\n", argv[0]);
-        return 1;
-    }
-
-    FILE* input = fopen(argv[1], "r");
-    FILE* output = fopen(argv[2], "w");
-    if (!input || !output) {
-        fprintf(stderr, "Erro ao abrir arquivos\n");
-        return 1;
-    }
-
-    int K, M;
-    if (fscanf(input, "%d", &K) != 1) { fprintf(stderr, "Erro ao ler K\n"); return 1; }
-
-    char DNA[10005];
-    if (fscanf(input, "%10004s", DNA) != 1) { fprintf(stderr, "Erro ao ler DNA\n"); return 1; }
-
-    if (fscanf(input, "%d", &M) != 1) { fprintf(stderr, "Erro ao ler M\n"); return 1; }
-    Doenca* d = (Doenca*)malloc(M * sizeof(Doenca));
-    if (!d) { perror("malloc"); return 1; }
-
-    for (int i = 0; i < M; i++) {
-        if (fscanf(input, "%9s %d", d[i].cod, &d[i].qtdGenes) != 2) {
-            fprintf(stderr, "Erro ao ler doença %d\n", i); return 1;
-        }
+    for (int i = 0; i < *M; i++) {
+        fscanf(input, "%9s %d", d[i].cod, &d[i].qtdGenes);
         d[i].genes = (char**)malloc(d[i].qtdGenes * sizeof(char*));
-        if (!d[i].genes) { perror("malloc"); return 1; }
         d[i].idx = i;
-        d[i].percent = 0;
+        d[i].porcentagem = 0;
 
         for (int j = 0; j < d[i].qtdGenes; j++) {
             d[i].genes[j] = (char*)malloc(1001);
-            if (!d[i].genes[j]) { perror("malloc"); return 1; }
-            if (fscanf(input, "%1000s", d[i].genes[j]) != 1) {
-                fprintf(stderr, "Erro ao ler gene %d da doença %s\n", j, d[i].cod); return 1;
-            }
+            fscanf(input, "%1000s", d[i].genes[j]);
         }
     }
 
-    // Cálculo correto: verificar cobertura de bases do gene
+    return d;
+}
+
+void exibir(FILE* output, Doenca* d, int M) {
+    for (int i = 0; i < M; i++) {
+        fprintf(output, "%s->%d%%\n", d[i].cod, d[i].porcentagem);
+    }
+}
+
+void calcularPorcentagens(Doenca* d, int M, char* Dna, int K) {
     for (int i = 0; i < M; i++) {
         int genesAtivos = 0;
 
         for (int g = 0; g < d[i].qtdGenes; g++) {
-            int lenGene = strlen(d[i].genes[g]);
+            int tamGene = strlen(d[i].genes[g]);
 
-            // Se o gene for menor que K, verificamos se o gene inteiro aparece
-            if (lenGene < K) {
+            if (tamGene < K) {
                 char* gene = d[i].genes[g];
                 int ktab[1001];
                 int Rcount = 0;
-                KMP(ktab, &Rcount, DNA, gene);
+                KMP(ktab, &Rcount, Dna, gene);
                 if (Rcount > 0) {
-                    genesAtivos++; // Gene inteiro encontrado
+                    genesAtivos++;
                 }
                 continue;
             }
 
-            // Para genes com comprimento >= K
-            int* coberto = (int*)calloc(lenGene, sizeof(int));
-            int numSub = lenGene - K + 1;
+            int* coberto = (int*)calloc(tamGene, sizeof(int));
+            int qtdSubcadeias = tamGene - K + 1;
 
-            // Para cada substring de tamanho K no gene
-            for (int pos = 0; pos < numSub; pos++) {
+            for (int pos = 0; pos < qtdSubcadeias; pos++) {
                 char sub[1001];
                 memcpy(sub, d[i].genes[g] + pos, K);
                 sub[K] = '\0';
 
-                // Verifica se esta substring aparece no DNA
                 int ktab[1001];
                 int Rcount = 0;
-                KMP(ktab, &Rcount, DNA, sub);
+                KMP(ktab, &Rcount, Dna, sub);
 
-                // Se aparecer, marca as posições cobertas
                 if (Rcount > 0) {
                     for (int k = 0; k < K; k++) {
                         coberto[pos + k] = 1;
@@ -153,36 +126,52 @@ int main(int argc, char* argv[]) {
                 }
             }
 
-            // Conta quantas posições foram cobertas
-            int totalCoberto = 0;
-            for (int p = 0; p < lenGene; p++) {
-                if (coberto[p]) totalCoberto++;
+            int total = 0;
+            for (int p = 0; p < tamGene; p++) {
+                if (coberto[p]) total++;
             }
 
-            // Verifica se pelo menos 90% das posições foram cobertas
-            float percentualCoberto = (float)totalCoberto / (float)lenGene * 100.0;
-            if (percentualCoberto >= 90.0) {
+            float porcentagemualCoberto = (float)total / (float)tamGene * 100.0;
+            if (porcentagemualCoberto >= 90.0) {
                 genesAtivos++;
             }
 
             free(coberto);
         }
 
-        // Calcula porcentagem final da doença
-        d[i].percent = round((100.0 * genesAtivos / d[i].qtdGenes));
+        d[i].porcentagem = round((100.0 * genesAtivos / d[i].qtdGenes));
     }
+}
 
-    mergesort(d, 0, M - 1);
-
+void liberarMemoria(Doenca* d, int M) {
     for (int i = 0; i < M; i++) {
-        fprintf(output, "%s->%d%%\n", d[i].cod, d[i].percent);
-    }
-
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < d[i].qtdGenes; j++) free(d[i].genes[j]);
+        for (int j = 0; j < d[i].qtdGenes; j++) {
+            free(d[i].genes[j]);
+        }
         free(d[i].genes);
     }
     free(d);
+}
+
+int main(int argc, char* argv[]) {
+    printf("#ARGS = %i\n", argc);
+    printf("PROGRAMA = %s\n", argv[0]);
+    printf("ARG1 = %s, ARG2 = %s\n", argv[1], argv[2]);
+
+    FILE* input = fopen(argv[1], "r");
+    FILE* output = fopen(argv[2], "w");
+
+    int K, M;
+    char Dna[10005];
+
+    Doenca* d = processarEntrada(input, &K, Dna, &M);
+    calcularPorcentagens(d, M, Dna, K);
+
+    Doenca* ordenar = (Doenca*)malloc(M*sizeof(Doenca));
+    counting_sort(d, ordenar, M);
+    exibir(output, d, M);
+    liberarMemoria(d, M);
+
     fclose(input);
     fclose(output);
     return 0;
