728e5de4d8160a09c1dfd1f50fbb0161a382b99a
Hugo Paixao <hs8622436@gmail.com>
2026-01-20 14:40:39 -0300

    d

--- projetos/labirinto/labirinto.c
+++ projetos/labirinto/labirinto.c
@@ -10,12 +10,12 @@ typedef struct casa{
 
 // o terceiro parâmetro servirá pra ver de contém ou nao um 'X'
 Casa processarLabirinto(int linha, int coluna, char tabuleiro[linha][coluna], Casa qtdCasas, FILE* input) {
-  for(int i = 0; i < linhas; i++) {
-    for(int j = 0; j < colunas; j++) {
+  for(int i = 0; i < linha; i++) {
+    for(int j = 0; j < coluna; j++) {
       fscanf(input, &tabuleiro[i][j]);
         if(tabuleiro[i][j] == 'X') {
-          casa1.qtdCasas = i;
-          casa2.qtdCasas = j;
+          qtdCasas.casa1 = i;
+          qtdCasas.casa2 = j;
         }
 
     }
@@ -26,7 +26,7 @@ Casa processarLabirinto(int linha, int coluna, char tabuleiro[linha][coluna], Ca
 int bin;
 
 int detectarEspaco(int linha, int coluna, char tabuleiro[linha][coluna], int x, int y) {
-  if(x >= 0 && (x < linha && y = >= 0 && y < coluna && tabuleiro[x][y] == '0'))
+  if(x >= 0 && x < linha && y = >= 0 && y < coluna && (tabuleiro[x][y] == '0'))
     return 1;
 
   return 0;
@@ -34,7 +34,7 @@ int detectarEspaco(int linha, int coluna, char tabuleiro[linha][coluna], int x, 
 
 int resolverLabirinto(int linha, int coluna, char tabuleiro[linha][coluna], int x, int y, FILE* output) {
   if(tabuleiro[linha][coluna] == '0' && (x == 0 || y == 0 || x == linha-1 || y = coluna-1)) {
-    fprintf(output, "FIM@%d,%d", x, y);
+    fprintf(output, "FIM@%d,%d\n", x, y);
     bin = 1;
     return 0;
   }
@@ -46,16 +46,69 @@ int resolverLabirinto(int linha, int coluna, char tabuleiro[linha][coluna], int 
     resolverLabirinto(linha, coluna, tabuleiro, x, y+1, output);
 
     if(bin != 1)
-      fprintf(output, "BT->%d,%d->%d,%d", x, y, x, y+1);
+      fprintf(output, "BT@%d,%d->%d,%d", x, y, x, y+1);
+    else
+      return 0;
+  }
+
+  if(detectarEspaco(linha, coluna, tabuleiro, x+1, y) == 1) {
+    fprintf(output, "F->%d,%d", x, y);
+    resolverLabirinto(linha, coluna, tabuleiro, x+1, y, output);
+
+    if(bin != 1)
+      fprintf(output, "BT@%d,%d->%d,%d", x, y, x+1, y);
     else
       return 0;
   }
 
   if(detectarEspaco(linha, coluna, tabuleiro, x, y-1) == 1) {
     fprintf(output, "E->%d,%d", x, y);
+    resolverLabirinto(linha, coluna, tabuleiro, x, y-1, output);
 
-    // desenhar uma "rosa dos ventos no papel pra ficar melhor de vizualizar"
+    if(bin != 1)
+      fprintf(output, "BT@%d,%d->%d,%d", x, y, x, y-1);
+    else
+      return 0;
+  }
+
+  if(detectarEspaco(linha, coluna, tabuleiro, x-1, y) == 1) {
+    fprintf(output, "T->%d,%d", x, y);
+    resolverLabirinto(linha, coluna, tabuleiro, x-1, y, output);
+
+    if(bin != 1)
+      fprintf(output, "BT@%d,%d->%d,%d", x, y, x-1, y, output);
+    else
+      return 0;
   }
 }
 
 
+int main(int argc, char* argv[]) {
+  printf("#ARGS = %i\n", argc);
+  printf("PROGRAMA = %s\n", argv[0]);
+  printf("ARG1 = %s, ARG2 = %s\n", argv[1], argv[2]);
+
+  FILE* input = fopen(argv[1], "r");
+  FILE* output = fopen(argv[2], "w");
+
+  int qtdLab, linha, coluna, i;
+  fscanf(input, "%d", &qtdLab);
+
+  for(i = 0; i < linha; i++) {
+    fscanf(output, "%d", coluna);
+    char tabuleiro[linha][coluna];
+    Casa casa;
+    casa = processarLabirinto(linha, coluna, tabuleiro, casa, input);
+    fprintf(output, "L%d:", i);
+    fprintf(output, "INI@%d,%d", linha);
+    resolverLabirinto(linha, coluna, tabuleiro, casa.casa1, casa.casa2);
+      if(bin == 0)
+        fprintf(output, "FIM@-,-\n");
+      bin = 0;
+  }
+
+  fclose(input);
+  fclose(output);
+
+  return 0;
+}
